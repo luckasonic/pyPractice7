b51fa9fa6e2c2947c8e96f2e490a85b57656dc77
luckasonic <151017044+luckasonic@users.noreply.github.com>
2025-04-02 16:57:23 +0300

    I/O functions signatures

---
+++ app/io/input.py
@@ -0,0 +1,9 @@
+def read_text_from_console():
+    pass
+
+def read_text_from_file(filepath):
+    pass
+
+def read_text_from_file_with_pandas(filepath):
+    pass
+
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def print_text_to_console(text):
+    pass
+
+def write_text_to_file(filepath, text):
+    pass
